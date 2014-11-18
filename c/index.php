84ce53adcb12f226b7dd94762c8875dd101c5c02
Pedro G. <japabrasuka@gmail.com>
2014-11-18 16:55:18 -0200

    Versão final do Index

--- c/index.php
+++ c/index.php
@@ -9,7 +9,20 @@ and open the template in the editor.
         <?php include './header.php'; ?>
     </head>
     <body>
-        <?php include './menu.php'; ?>
-        <?php include '../c/footer.php';?>
+        <div class="container">
+            <?php include './menu.php'; ?>
+            <div class="row">
+                <div class="col-xs-10 col-lg-offset-1">
+                    <h1 class="text-center">Bem vindo ao curso de C</h1>
+                    <p class="text-justify">
+                        Este site foi desenvolvido por um aluno do Colégio Técnico 
+                        da UFMG com intuito de tornar o hábito de programação mais presente
+                        na vida dos alunos da escola.<br/>
+                        OBS: Nós não temos bolo.
+                    </p>
+                </div>
+            </div> 
+            <?php include '../c/footer.php'; ?>
+        </div>
     </body>
 </html>
