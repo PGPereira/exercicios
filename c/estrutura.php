2ccf3ec6dcde2fb511a6e0a4a7f2ee2f59db7c4b
Pedro G. <japabrasuka@gmail.com>
2014-11-18 17:01:46 -0200

    Final

---
+++ c/estrutura.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <?php include './header.php'; ?>
+    </head>
+    <body>
+        <div class="container">
+            <?php include './menu.php'; ?>
+            <div class="row">
+                <div class="col-xs-10 col-lg-offset-1">
+                    <h1 class="text-center">Estrutura</h1>
+                    <p class="text-justify">
+                        O curso é dividido em tópicos de uma maneira um tanto quanto arbitrária. <br/>
+                        Um tópico tem por intenção tratar de todas as peculiaridades de um determinado assunto. <br/>
+                        Caso seja necessário o tópico se dividirá em sub tópicos. <br/>
+                    </p>
+                </div>
+            </div> 
+            <?php include '../c/footer.php'; ?>
+        </div>
+    </body>
+</html>
