59b1ee26e379b341d48fdf37c99832aec36f2cd7
Pedro G. <japabrasuka@gmail.com>
2014-11-18 17:03:55 -0200

    Edição final do arquivo de navegação

---
+++ c/navegacao.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <?php include './header.php'; ?>
+    </head>
+    <body>
+        <div class="container">
+            <?php include './menu.php'; ?>
+            <div class="row">
+                <div class="col-xs-10 col-lg-offset-1">
+                    <h1 class="text-center">Navegação</h1>
+                    <p class="text-justify">
+                        Toda vez que você clicar em um link que tem um sub-tópico abrirá uma página com o significado do tópico principal. <br/>
+                        No mais basta clicar no link nos menus e ler o conteúdo. <br/>
+                        Sim, é simples assim.
+
+                    </p>
+                </div>
+            </div> 
+            <?php include '../c/footer.php'; ?>
+        </div>
+    </body>
+</html>
