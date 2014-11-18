a7fb9ed0048733aec9f3b3e11309ac2ec8c195d0
Pedro G. <japabrasuka@gmail.com>
2014-11-18 17:40:41 -0200

    Acabou a porra toda

--- globo/index.php
+++ globo/index.php
@@ -17,14 +17,38 @@ and open the template in the editor.
                 <p class="text-center">Nem parece</p>
             </div>
             <div class="row">
-                <div class="col-sm-12">
+                <div class="col-sm-12 thumbnail">
                     <div class="col-xs-4">
-                        <image src="../images/13.jpg"/>
+                        <img src="../images/45186298.gif" alt="Manchete" class="img-responsive img-thumbnail">
                     </div>
                     <div class="col-xs-8">
-                        <p> Crime ocorre </p>
-                        <p> Nada acontece </p>
-                        <p> Feijoada </p>
+                        <h1 class="text-center">Profissão de hacker está em alta.</h1>
+                        <p class="text-justify"> 
+                            Here comes the Kelvin Mitnick in the curve of the hill belonged to grandma Savelina.<br/>
+                            Throw him the mysql injection, Kelvin Mitnick.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="row">
+                        <div class="col-sm-4 thumbnail">
+                            <img src="../images/44417920.png" alt="Manchete" class="img-responsive img-thumbnail">
+                            <h3 class="text-center">Descoberto o sentido da vida</h3>
+                            <p class="text-justify">E ele inclui motivar pessoas no 4chan, dizem especialistas</p>
+                            <p class="text-center"><a href="" class="btn btn-primary" role="button">Read more</a></p>
+                        </div>
+                        <div class="col-sm-4 thumbnail">
+                            <img src="../images/42813584.png" alt="Manchete" class="img-responsive img-thumbnail">
+                            <h3 class="text-center">Primeiro diagrama de Venn sobre namoros do ano sai</h3>
+                            <p class="text-justify">E não traz consigo uma perspectiva muito animadora</p>
+                            <p class="text-center"><a href="" class="btn btn-primary" role="button">Read more</a></p>
+                        </div>
+                        <div class="col-sm-4 thumbnail">
+                            <img src="../images/44207781.jpg" alt="Manchete" class="img-responsive img-thumbnail">
+                            <h3 class="text-center">Barba de usuários Linux estudada</h3>
+                            <p class="text-justify">O mistério por trás dessas grandissíssimas barbas foi enfim desmistificado</p>
+                            <p class="text-center"><a href="" class="btn btn-primary" role="button">Read more</a></p>
+                        </div>
                     </div>
                 </div>
             </div>
