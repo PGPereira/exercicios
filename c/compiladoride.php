1cff84f231470c5fd359ce166e189f7237e11260
Pedro G. <japabrasuka@gmail.com>
2014-11-18 16:59:35 -0200

    Final

--- c/compiladoride.php
+++ c/compiladoride.php
@@ -21,7 +21,7 @@ and open the template in the editor.
                         Um programa que traduz uma linguagem de programação de baixo nível para uma linguagem de programação de alto nível é um descompilador.6 Um programa que faz uma tradução entre uma linguagem de montagem e o código de máquina é denominado montador (assembler).5 Um programa que faz uma tradução entre o código de máquina e uma linguagem de montagem é denominado desmontador (disassembler). <br/>
                         Se o programa compilado pode ser executado em um computador cuja CPU ou sistema operacional é diferente daquele em que o compilador é executado, o compilador é conhecido como um compilador cruzado.
                     </p>
-                    <h1>IDE</h1>
+                    <h1 class="text-center">IDE</h1>
                     <p class="text-justify">
                         IDE, do inglês Integrated Development Environment ou Ambiente Integrado de Desenvolvimento, é um programa de computador que reúne características e ferramentas de apoio ao desenvolvimento de software com o objetivo de agilizar este processo. <br/>
                         Geralmente os IDEs facilitam a técnica de RAD (de Rapid Application Development, ou "Desenvolvimento Rápido de Aplicativos"), que visa a maior produtividade dos desenvolvedores.
