e2369563eb8c4f49b63c55603f1a3bcfadb43d00
Pedro G. <japabrasuka@gmail.com>
2014-11-18 16:55:37 -0200

    Versão final do Menu

--- c/menu.php
+++ c/menu.php
@@ -8,35 +8,35 @@
 <div class="navbar navbar-static">
     <div class="navbar-inner">
         <ul class="nav nav-pills">
-            <li class="active"><a href="#">Home</a></li>
+            <li class="active"><a href="index.php">Home</a></li>
             <li class="dropdown" >
                 <a href="#" data-toggle="dropdown" class="dropdown-toggle">Tópicos<b class="caret"></b></a>
                 <ul class="dropdown-menu">
-                    <li><a href="#">Linguagem C</a></li>
+                    <li><a href="linguagem.php">Linguagem C</a></li>
                 </ul>
             </li>
             <li class="dropdown">
                 <a href="#" data-toggle="dropdown" class="dropdown-toggle">Extras <b class="caret"></b></a>
                 <ul class="dropdown-menu">
-                    <li><a href="#">Compiladores & IDEs</a></li>
-                    <li><a href="#">Aplicações</a></li>
-                    <li><a href="#">Sistema Operacional</a></li>
+                    <li><a href="compiladoride.php">Compiladores & IDEs</a></li>
+                    <li><a href="aplicacoes.php">Aplicações</a></li>
+                    <li><a href="os.php">Sistema Operacional</a></li>
                 </ul>
             </li>
             <li class="dropdown">
                 <a href="#" data-toggle="dropdown" class="dropdown-toggle">Downloads<b class="caret"></b></a>
                 <ul class="dropdown-menu">
-                    <li><a href="#">Exemplos</a></li>
-                    <li><a href="#">Tutoriais</a></li>
-                    <li><a href="#">Artigos</a></li>
+                    <li><a href="exemplos.php">Exemplos</a></li>
+                    <li><a href="tutoriais.php">Tutoriais</a></li>
+                    <li><a href="artigos.php">Artigos</a></li>
                 </ul>
             </li>
             <li class="dropdown">
                 <a href="#" data-toggle="dropdown" class="dropdown-toggle">Ajuda<b class="caret"></b></a>
                 <ul class="dropdown-menu">
-                    <li><a href="#">Contato</a></li>
-                    <li><a href="#">Navegação</a></li>
-                    <li><a href="#">Estrutura do Curso</a></li>
+                    <li><a href="../cv/index.php">Contato</a></li>
+                    <li><a href="navegacao.php">Navegação</a></li>
+                    <li><a href="estrutura.php">Estrutura do Curso</a></li>
                 </ul>
             </li>
         </ul>
