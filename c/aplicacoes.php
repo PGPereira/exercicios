21f5a615788d07946f3219364b412ff7dd644289
Pedro G. <japabrasuka@gmail.com>
2014-11-18 16:54:51 -0200

    Aplicações em C

---
+++ c/aplicacoes.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <?php include './header.php'; ?>
+    </head>
+    <body>
+        <div class="container">
+            <?php include './menu.php'; ?>
+            <div class="row">
+                <div class="col-xs-10 col-lg-offset-1">
+                    <h1 class="text-center">Aplicação</h1>
+                    <p class="text-justify">
+                        C é uma linguagem de programação de alto desempenho e baixa
+                        produtividade, pois permite controle de variáveis que se aproxima
+                        bastante da linguagem Assembly. <br/>
+                    </p>
+                    <h1 class="text-center">Programas desenvolvidos em C:</h1>
+                    <ul class="text">
+                        <li> Adobe System </li>
+                        <li> Google </li>
+                        <li> Mozilla </li>
+                        <li> MySQL </li>
+                        <li> Alias System - Autodesk Maya</li>
+                        <li> Winamp Media Player </li>
+                        <li> 12D Solutions </li>
+                        <li> Bloomberg </li>
+                        <li> callas Software </li>
+                        <li> Apple - Os X </li>
+                        <li> Microsoft </li>
+                        <li> Symbian OS </li>
+                    </ul>
+                </div>
+            </div> 
+            <?php include '../c/footer.php'; ?>
+        </div>
+    </body>
+</html>
+
+
